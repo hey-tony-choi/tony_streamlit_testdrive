e358cc64c39c3968581d509cdcd5ab26753068ba
TonyChoi <133308718+hey-tony-choi@users.noreply.github.com>
2025-08-15 06:09:40 +0000

    edit images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My second app")
+st.title("🎈 Make app great again")
 st.write(
     "Let's start building!"
 )
 
-st.subheader("image section")
-st.image("https://static.streamlit.io/examples/cat.jpg", caption="귀여운 고양이", use_container_width=True)
+st.subheader("1.image section")
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWhvN3EydGxnYmY5cWl4aHQzbXFmaDN5M2NiZnJ6YnAxczFyejM3NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/WrIiyrvaFSeyVlxwtx/giphy.gif", caption="You're the best", use_container_width=True)
 
+st.subheader("2. code section")
 st.code("""
 import streamlit as st
 st.title('Hello World')
