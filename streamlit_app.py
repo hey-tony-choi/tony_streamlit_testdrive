7570ba2377ecfb73375b7f1302db341090056c2d
TonyChoi <133308718+hey-tony-choi@users.noreply.github.com>
2025-08-15 06:00:03 +0000

    tinman!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My second app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building!"
 )
+
+st.subheader("image section")
+st.image("https://static.streamlit.io/examples/cat.jpg", caption="귀여운 고양이", use_container_width=True)
+
+st.code("""
+import streamlit as st
+st.title('Hello World')
+""", language="python")
+
